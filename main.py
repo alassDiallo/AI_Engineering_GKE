e0bb4e991c3bf31b58d18bf27223b46756d83142
assane <alassdiallo58@gmail.com>
2026-05-27 15:47:49 +0200

    ajoute dossier

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+class User(BaseModel):
+    name: str
+    age: int
+
+app = FastAPI(title="API test")
+
+@app.get("/")
+def accueil():
+    return {"message": "Hello World!"}
+
+
+@app.post("/informations")
+def informations(user:User):
+    return {"message": f"Hello {user.name}, you are {user.age} years old!"}
