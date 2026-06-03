f4d118b20586d3c6640d661ad8d1d5cd8934672b
assane <alassdiallo58@gmail.com>
2026-06-03 11:33:10 +0200

    ajout des endpoint get et post

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from db import get_mysql_host
+from db import CommentDB
+
+comment_db = CommentDB()
 
 
 class User(BaseModel):
@@ -24,5 +26,28 @@ def informations(user: User):
 
 @app.get("/dbconnection")
 def dbconnection():
-    db = get_mysql_host()
-    return {"message": "Connected to database!", "dbInstance": db}
+    return {"message": "Connected to database!", "dbInstance": comment_db}
+
+
+@app.post("/get_comments")
+def get_comments():
+    """
+    get_comments retourne tous les commentaires
+    :return: retourne tous les comentaires
+    """
+    return comment_db.get_comments()
+
+
+@app.post("/add_comment")
+def add_comment(name: str, comment: str):
+    """
+    add_comment ajoute un commentaire a la base de données
+    :param name: le nom de l'utilisateur qui a fait le commentaire
+    :param comment: le commentaire de l'utilisateur
+    :return: retourne un message de succès ou d'erreur
+    """
+    from datetime import date
+
+    today = date.today()
+
+    return comment_db.add_comment(name, comment, today.strftime("%Y-%m-%d"))
